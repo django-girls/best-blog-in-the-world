68af12a57a11338afc419ee15e32cf24be0818c6
Paulina Szczerbak <paulinaszczerbak@gmail.com>
2017-09-17 14:35:45 +0200

    new view - Paula

--- Paula/urls.py
+++ Paula/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^new_view$', views.new_view, name='new_view'),
 ]
--- Paula/views.py
+++ Paula/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def new_view(request):
+    return render(request, 'Paula/post_list.html', {})
